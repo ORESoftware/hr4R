a8b036ffb97e9dfa8258812494a23961fdb491f8
Alexander Mills <alexander_mills2@cable.comcast.com>
2015-06-19 20:24:36 -0700

    app.js

--- app.js
+++ app.js
@@ -159,10 +159,8 @@ app.use(function (req, res, next) {
     next(err);
 });
 
-// error handlers
 
 // development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function (err, req, res, next) {
         res.status(err.status || 500);
@@ -174,7 +172,6 @@ if (app.get('env') === 'development') {
 }
 
 // production error handler
-// no stacktraces leaked to user
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
